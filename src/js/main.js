ff22de8933ee0e53c27e5f4e58b5562077b9c8c3
mepeha <151367905+mepeha@users.noreply.github.com>
2024-12-02 22:05:45 +0300

    for vanya

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.main-banner__area').slick({
+
+    });
+    $('.more-client__area').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    });
+});
